913c59969b69406e8434793fe29d44f09967633b
khalilbego1 <khalilbego1@etf.unsa.ba>
2019-05-12 16:06:52 +0200

    change status form added

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { ChangeStatusComponent } from './change-status/change-status.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, NgbModule],
+  declarations: [ AppComponent, HelloComponent, WrapperComponent, ChangeStatusComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
