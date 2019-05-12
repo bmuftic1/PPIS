a02bff4bee3a8a7724683ffe11261101eceaed97
ikubat1 <ikubat1@etf.unsa.ba>
2019-05-12 17:12:46 +0200

    Added helpdesk changes and developer forms

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -7,10 +7,13 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
 import { ChangeStatusComponent } from './change-status/change-status.component';
+import { DeveloperContentComponent } from './developer-content/developer-content.component';
+import { DeveloperWrapperComponent } from './developer-wrapper/developer-wrapper.component';
+import { HelpdeskChangesComponent } from './helpdesk-changes/helpdesk-changes.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, NgbModule],
-  declarations: [ AppComponent, HelloComponent, WrapperComponent, ChangeStatusComponent ],
+  declarations: [ AppComponent, HelloComponent, WrapperComponent, ChangeStatusComponent, DeveloperContentComponent, DeveloperWrapperComponent, HelpdeskChangesComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
