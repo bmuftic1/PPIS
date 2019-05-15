72cedf372124c5860ad9eb1867e5c78952c7e9aa
khalilbego1 <khalilbego1@etf.unsa.ba>
2019-05-15 20:07:32 +0200

    Added dropdowns to status form

--- FrontEnd/src/app/change-status/change-status.component.ts
+++ FrontEnd/src/app/change-status/change-status.component.ts
@@ -8,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ChangeStatusComponent implements OnInit {
 
   constructor() { }
+  StatusStyle = "form-control form-status-cekanje";
+  StatusText ="Čekanje"
 
+  Statuses : any[] = [
+    {text: "Čekanje", style:"form-control form-status-cekanje"},
+    {text: "Greška", style:"form-control form-status-greska"},
+    {text: "U toku", style: "form-control form-status-uToku"},
+    {text: "Završeno", style:"form-control form-status-zavrseno"}
+  ]
   ngOnInit() {
+  }
+  setStatus(status:any){
+    this.StatusStyle = status.style;
   }
 
 }
