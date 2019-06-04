e9eaf6487472437ebf95bbb74f6a1595d5987212
belmagigi <belmamuf@gmail.com>
2019-06-04 23:14:35 +0200

    Adding routes for frontend

--- FullBackend/routes/izvjestaj.router.js
+++ FullBackend/routes/izvjestaj.router.js
@@ -102,4 +102,38 @@ router.get("/brojKorisnika", (req, res, next) => {
   });
 
 });
+
+
+router.get("/promjenecr", (req, res, next) => {
+  let id = req.params.id;
+  var sql = `SELECT * FROM Promjenas where opis like '%cekanje%' or opis like '%rjesavanje%'`;
+  db.query(sql, function(err, rez) {
+	  if (err) console.log(err);
+	  else res.status(200).send(rez);
+  });
+
+});
+
+
+router.get("/promjenekategorija", (req, res, next) => {
+  let id = req.params.id;
+  var sql = `SELECT k.nazivKategorije, count(p.kategorijaPromjeneId) broj FROM KategorijaPromjenes k, Promjenas p where k.id=p.kategorijaPromjeneId group by k.nazivKategorije`;
+  db.query(sql, function(err, rez) {
+	  if (err) console.log(err);
+	  else res.status(200).send(rez);
+  });
+
+});
+
+
+router.get("/tipovidogadjaja", (req, res, next) => {
+  let id = req.params.id;
+  var sql = `SELECT t.tipDogadjaja, count(d.tipId) broj FROM Dogadjajs d, TipDogadjajas t where t.id=d.tipId group by t.id`;
+  db.query(sql, function(err, rez) {
+	  if (err) console.log(err);
+	  else res.status(200).send(rez);
+  });
+
+});
+
 module.exports = router;
